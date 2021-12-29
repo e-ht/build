2160ff4840bfa6f12cd4bd773dcd9e2e21d4b8d3
e-ht <1045813+e-ht@users.noreply.github.com>
2021-12-29 15:01:22 -0800

    +nginx conf automate +certbot SSL automate

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ todo
 - env processing for future bjorg implemetation
 - tidy up bird bgp route setup
 - automate network interface creation and route assign 
+- use ```SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )``` for safely assuming our
+place in life and chaining scripts
+
+### Firewall todo:
+
+- for ip addressing with BIRD, ```DENY ALL``` for institutional IP (maybe)
+  - consider whitelist access only
--- debian11/bash/services/nginx/web.sh
+++ debian11/bash/services/nginx/web.sh
@@ -2,6 +2,9 @@
 HISTSIZE=0
 set +o history
 
+FQDN=$1
+CERTBOT_EMAIL=$2
+
 echo "SETUP UP WEB NOW OK THANKS"
 # TODO set rlimits in nginx conf as well as for base system
 # TODO set file paths as variables in head of file?
@@ -12,7 +15,7 @@ apt install -y nginx certbot python3-certbot-nginx
 ufw allow 80
 ufw allow 443
 
-# ensure nolimit is respected in nginx.conf
+# ensure nolimit with systemd
 mkdir -p /etc/systemd/system/nginx.service.d
 touch /etc/systemd/system/nginx.service.d/nginx.conf
 cat <<CHUNK >> /etc/systemd/system/nginx.service.d/nginx.conf
@@ -22,22 +25,25 @@ CHUNK
 systemctl daemon-reload
 systemctl restart nginx.service
 
-#
-## Transfer templates
-#echo "Transferring templates..."
-#mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
-#cat templates/nginx.conf > /etc/nginx/nginx.conf
-#cat templates/nginx.default > /etc/nginx/sites-available/default
+# Transfer templates
+echo "Backing up nginx.conf ..."
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
+echo "Injecting templates ..."
+cat nginx.conf > /etc/nginx/nginx.conf
+cat default > /etc/nginx/sites-available/default
+cat index.html > /var/www/html/index.html
+
+# be url specific if you wanna
 #sed -i 's/server_name _.*/server_name '"$FQDN"';/' /etc/nginx/sites-available/default
-#cat templates/index.html > /var/www/html/index.html
-#
-## Full stop start
-#service nginx stop
-#service nginx start
-#
-## Request SSL
-## Try for --hsts and --uir even though they aren't currently supported with the nginx module
-#echo "Setting up SSL... ?"
-##certbot -n --agree-tos --email "$CERTBOT_EMAIL" --authenticator webroot -w /var/www/html/ --installer nginx --redirect --hsts --uir --domain "$FQDN"
-#
-##service nginx reload
+
+# Full stop start
+service nginx stop
+service nginx start
+
+# Request SSL
+# Try for --hsts and --uir even though they aren't currently supported with the nginx module
+echo "Setting up SSL ..."
+certbot -n --agree-tos --email "$CERTBOT_EMAIL" --authenticator webroot -w /var/www/html/ --installer nginx --redirect --hsts --uir --domain "$FQDN"
+
+service nginx reload
+echo "Done ...?"
